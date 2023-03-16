5f59936df50da449c3289c4dc7c2756d1177a103
Adeyemi Adedugbe <deyemsco12@yahoo.co.uk>
2023-03-16 22:59:35 +0100

    EXECUTABLE!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success!)
+ */
+
+int main(void)
+{
+  printf("with propr grammar, but the outcom is a piece of art,\n");
+  return (0);
+}
